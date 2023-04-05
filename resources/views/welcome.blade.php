17b4c3cd71a6e61dbeb179196bdb833a24d92ef4
MohammadAdnanAldaas <71332210+MohammadAdnanAldaas@users.noreply.github.com>
2023-04-05 09:56:25 +0200

    Minor addup
    make special menus clickable

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -123,7 +123,11 @@
                             <div class="flex mb-2">
                                 <span class="px-4 py-0.5 text-sm bg-red-500 rounded-full text-red-50">Special menu</span>
                             </div>
-                            <h4 class="mb-3 text-xl font-semibold tracking-tight text-green-600 uppercase">{{ $menu->name }}</h4>
+                            <a href="{{ route('menus.show', $menu->id) }}">
+                                <h4 class="mb-3 text-xl font-semibold tracking-tight text-green-600 hover:text-green-400 uppercase">
+                                    {{ $menu->name }}
+                                </h4>
+                            </a>
                             <p class="leading-normal text-gray-700">{{ $menu->description }}.</p>
                         </div>
                         <div class="flex items-center justify-between p-4">
