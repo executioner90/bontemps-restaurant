cc1bdda03148c913de225bec9840ee8437eccf3f
mohammad <mohammad@inktweb.nl>
2024-09-21 11:04:07 +0200

    minor fix

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -59,8 +59,7 @@
                                 <label for="text" class="block mb-2 text-gray-700 font-bold">Message</label>
                                 <textarea id="text"
                                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                       placeholder="What's on your mind?">
-                                </textarea>
+                                       placeholder="What's on your mind?"></textarea>
                             </div>
                             <button type="submit"
                                     class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus-shadow-outline">
