ddcfc219dedcd77380b866d6c1bf4e8885f746b8
mohammad <mohammad@inktweb.nl>
2024-09-21 11:15:25 +0200

    add invalid class to email input

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -42,22 +42,25 @@
                                 HOT
                             </div>
                         </div>
-                        <form class="space-y-4">
+                        <form class="space-y-6">
                             <div>
                                 <label for="name" class="block mb-2 text-gray-700 font-bold">Name</label>
                                 <input type="text" id="name"
+                                       required
                                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:border-green-400 focus:shadow-outline"
                                        placeholder="Zach S.">
                             </div>
                             <div>
                                 <label for="email" class="block mb-2 text-gray-700 font-bold">Email</label>
                                 <input type="email" id="email"
-                                       class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                       required
+                                       class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline invalid:[&:not(:placeholder-shown)]:border-red-500"
                                        placeholder="example@email.com">
                             </div>
                             <div>
                                 <label for="text" class="block mb-2 text-gray-700 font-bold">Message</label>
                                 <textarea id="text"
+                                          required
                                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                        placeholder="What's on your mind?"></textarea>
                             </div>
