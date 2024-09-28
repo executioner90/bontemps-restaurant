a7c0adbae7ed6c91af0eff96a8dba74fd87a1774
mohammad <mohammad@inktweb.nl>
2024-09-28 12:30:46 +0200

    menus page restructured

--- resources/views/components/frontend/home/menus/special.blade.php
+++ resources/views/components/frontend/home/menus/special.blade.php
@@ -7,26 +7,7 @@
 <div class="container py-6">
     <div class="grid lg:grid-cols-4 gap-7">
         @foreach($specials as $menu)
-            <div class="mb-2 rounded-lg shadow-lg">
-                <a href="{{ route('menus.show', $menu->id) }}">
-                    <img class="w-full h-48" src="{{ asset(Storage::url($menu->image)) }}"
-                         alt="Image"/>
-                    <div class="px-6 py-4">
-                        <div class="flex mb-2">
-                                    <span
-                                        class="px-4 py-0.5 text-sm bg-red-500 rounded-full text-red-50">Special menu</span>
-                        </div>
-                        <h4 class="mb-3 text-xl font-semibold tracking-tight text-green-600 hover:text-green-400 uppercase">
-                            {{ $menu->name }}
-                        </h4>
-
-                        <p class="leading-normal text-gray-700">{{ $menu->description }}.</p>
-                    </div>
-                    <div class="flex items-center justify-between p-4">
-                        <span class="text-xl text-green-600">&euro; {{ $menu->price  }}</span>
-                    </div>
-                </a>
-            </div>
+            <x-frontend.menus.block :menu="$menu"></x-frontend.menus.block>
         @endforeach
     </div>
 </div>
