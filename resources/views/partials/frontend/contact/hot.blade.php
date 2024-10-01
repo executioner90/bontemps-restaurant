38c06150ac5f9823b3ca448f25a9971369f54694
mohammad <mohammad@inktweb.nl>
2024-10-01 09:53:25 +0200

    contact page restructured

---
+++ resources/views/partials/frontend/contact/hot.blade.php
@@ -0,0 +1,14 @@
+<div class="flex justify-between items-center mb-4">
+    <div class="flex items-center">
+        <span class="text-sm font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">Bontemps</span>
+
+        <div class="ml-4">
+            <p class="font-bold text-lg">Customer service</p>
+            <p class="text-gray-500">+123 456 789</p>
+            <p class="text-gray-500">info@bontemps.nl</p>
+        </div>
+    </div>
+    <div class="bg-red-500 text-white font-bold px-3 py-1 rounded-full">
+        HOT
+    </div>
+</div>
