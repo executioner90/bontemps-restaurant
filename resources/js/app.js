f7a73001ac9e03aa38cf855a28269934ba9a84ae
mohammad <mohammad@inktweb.nl>
2024-10-09 13:23:54 +0200

    added vue && updated packages

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,28 @@
 import './bootstrap';
-
 import Alpine from 'alpinejs';
+import { createApp } from 'vue';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+// Register components
+const componentFiles = import.meta.glob('./components/**/*.vue', {
+    eager: true,
+    import: 'default',
+});
+
+const app = createApp({});
+
+Object.entries(componentFiles).forEach(([path, module]) => {
+    const componentName = path
+        .replace('./components/', '')
+        .replaceAll('/', '')
+        .replace('.vue', '');
+
+    console.log(`Registering component: ${componentName}`);
+    app.component(componentName, module);
+});
+
+// Mount the app
+app.mount('#app');
