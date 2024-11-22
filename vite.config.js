271a1b36b3410dcdae5d60fd7aebda1e6e36dab3
mohammad <mohammad@inktweb.nl>
2024-11-22 15:40:11 +0100

    Revert "updated form as needed"
    This reverts commit d7f34778ef26ca5f4091b8c422c6aa6ea3bd0705.

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/scss/app.scss',
+                'resources/css/app.css',
                 'resources/js/app.js',
             ],
             refresh: true,
