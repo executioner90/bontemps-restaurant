1ad55fde2fa6a023bd91c5e2a760b0efae457981
mohammad <mohammad@inktweb.nl>
2025-08-29 09:25:02 +0200

    Minor update

--- resources/views/components/frontend/header/breadcrumbs.blade.php
+++ resources/views/components/frontend/header/breadcrumbs.blade.php
@@ -1,5 +1,5 @@
 <div class="container flex items-center py-4 overflow-x-auto whitespace-nowrap">
-    <a href="{{ route('home') }}" class="@if(count($breadcrumbs) > 0)text-gray-500 dark:text-gray-200 hover:text-gray-600 @endif">
+    <a href="{{ route('home') }}" class="@if(!$isHome)text-gray-500 dark:text-gray-200 hover:text-gray-600 @endif">
         <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
             <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
         </svg>
